0e679c7e59c75b4650df0fb3d06c8aeca17a9ffe
vagrant <vagrant@localhost.localdomain>
2021-01-26 07:46:04 +0000

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.7'
 gem 'bootstrap-sass', '3.3.7'
 gem 'rails-controller-testing'
+gem 'bcrypt'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.4'
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+before_save { email.downcase! } # 保存する前に小文字に変換する
+  validates:name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # 文字列先頭、英数字_+-.のいずれかを最低1文字、＠、英数字-.を最低一文字、.、英数字を最低一文字、文字列の末尾、iで締める
+  validates:email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: {case_sensitive: false} # 大文字小文字どちらも同じと認識して一意性を保つ
+  has_secure_password # パスワードをハッシュ化し、password_digestに保存、authenticate（引数の文字列がパスワードと一致するとUserオブジェクトを、血がければfalseを返す）メソッド利用可
+  validates:password, presence: true, length: { minimum: 6 }
+end
