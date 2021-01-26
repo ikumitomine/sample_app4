1aa22d59f0b34bb935e9464848cc23a5c7b29f8f
vagrant <vagrant@localhost.localdomain>
2021-01-26 10:06:59 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params) # =params[:user] = 入力した情報のハッシュ形式
+    if @user.save
+      flash[:success] = "Wlcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    # user_paramsの中には以下のデータのみを許可して保存する。あとエバ、adminなどを追加して保存させ、アプリを操作されない用にする
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
